9a1cf5d9277a912c9e449e43bab6b896099cbc82
Angeline Polidano <angelinepolidano@gmail.com>
2023-01-30 11:34:13 -0800

    Add model for comments, user has ability to post a comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
 
 
-  resources :articles
+  resources :articles do
+    resources :comments
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  end
 end
